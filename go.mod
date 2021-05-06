61802e4883623fd300b7182039fe414646cd5615
TrickyTroll <tricky@hey.com>
2021-05-06 16:30:46 -0400

    Add: config files & styles

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/TrickyTroll/good-bot-app
+
+go 1.16
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  theme: {
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
+  },
+  variants: {
+    extend: {
+      borderColor: ['focus-visible'],
+      opacity: ['disabled'],
+    }
+  }
+}
